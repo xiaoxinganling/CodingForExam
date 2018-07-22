5053cfddb03e0c52623abcb1acb1acb9f6f965a5
zmxxx <1214026832@qq.com>
2018-07-22 17:37:31 +0800

    新增直径问题

---
+++ src/problem/ali/TreeDiameter.java
@@ -0,0 +1,13 @@
+package problem.ali;
+import java.util.*;
+public class TreeDiameter {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int pointNum = input.nextInt();
+        int edgeNum = input.nextInt();
+        input.nextLine();
+        int[][] matrix = new int[pointNum][pointNum];
+        int[] isVisited = new int[pointNum];
+
+    }
+}
